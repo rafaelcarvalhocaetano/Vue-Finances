f24177bec2d62351788414b17e91b7ee93b9e998
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2020-01-04 18:12:07 -0200

    Finished validate form

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -4,16 +4,28 @@
       <v-flex xs12 sm6 md4 lg13 x13>
         <v-card>
           <v-toolbar color="primary" dark>
-            <v-toolbar-title>Login</v-toolbar-title>
+            <v-toolbar-title>{{ texts.toolbar }}</v-toolbar-title>
           </v-toolbar>
           
             <v-card-text>
               <v-form>
                 <v-text-field
+                  v-if="!isLogin"
+                  prepend-icon="person"
+                  name="name"
+                  label="Nome"
+                  type="text"
+                  :error-messages="nameErrors"
+                  :success="!$v.user.name.$invalid"
+                  v-model.trim="$v.user.name.$model"
+                  ></v-text-field>
+                <v-text-field
                   prepend-icon="email"
                   name="email"
                   label="Email"
                   type="email"
+                  :error-messages="emailErrors"
+                  :success="!$v.user.email.$invalid"
                   v-model.trim="$v.user.email.$model"
                   ></v-text-field>
                 <v-text-field
@@ -21,12 +33,15 @@
                   name="password"
                   label="Senha"
                   type="password"
+                  :error-messages="passwordErrors"
+                  :success="!$v.user.password.$invalid"
                   v-model.trim="$v.user.password.$model"
                   ></v-text-field>
               </v-form>
               <v-btn
                 block depressed color="secondary"
-              >Criar Conta</v-btn>
+                @click="isLogin = !isLogin"
+              >{{ texts.button }}</v-btn>
 
             </v-card-text>
             <v-card-actions>
@@ -35,7 +50,7 @@
                 color="primary" large
                 :disabled="$v.$invalid"
                 @click="submit"
-              >Login</v-btn>
+              >{{ texts.toolbar }}</v-btn>
             </v-card-actions>
         </v-card>
       </v-flex>
@@ -50,27 +65,79 @@ import { required, email, minLength } from 'vuelidate/lib/validators'
 export default {
   name: 'Login',
   data: () => ({
+    isLogin: true,
     user: {
+      name: '',
       email: '',
       password: ''
     }
   }),
-  validations: {
-    user: {
-      email: {
-        required,
-        email
-      },
-      password: {
-        required,
-        minLength: minLength(6)
+  validations () {
+    const formValid = {
+      user: {
+        email: {
+          required,
+          email
+        },
+        password: {
+          required,
+          minLength: minLength(6)
+        }
       }
+    }
+    if (this.isLogin) {
+      return formValid
+    }
+    return {
+      user: {
+        ...formValid.user,
+        name: {
+          required,
+          minLength: minLength(4)
+        }
+      }
+    }
+  },
+  computed: {
+    texts () {
+      return this.isLogin ? { toolbar: 'Entrar', button: 'Criar conta' } : { toolbar: 'Criar conta', button: 'Já tenho uma conta' }
+    },
+    nameErrors () {
+      const errors = []
+      const name = this.$v.user.name
+      if (!name.$dirty) {
+        return errors
+      }
+      !name.required && errors.push('Nome é obrigatório')
+      !name.minLength && errors.push(`Insira pelo menos ${name.$params.minLength.min} caracteres`)
+      return errors
+    },
+    emailErrors () {
+      const errors = []
+      const email = this.$v.user.email
+      if (!email.$dirty) {
+        return errors
+      }
+      !email.required && errors.push('Email é obrigatório')
+      !email.email && errors.push('Insira um email válido')
+      return errors
+    },
+    passwordErrors () {
+      const errors = []
+      const password = this.$v.user.password
+      if (!password.$dirty) {
+        return errors
+      }
+      !password.required && errors.push('Password é obrigatório')
+      !password.minLength && errors.push(`Insira pelo menos ${password.$params.minLength.min} caracteres`)
+      return errors
     }
   },
   methods: {
     submit () {
       console.log(' data ', this.$v)
       console.log(' user ', this.user)
+      console.log(' isLogin ', this.isLogin)
     }
   }
 }

a157214f82ce92d6e2404fe555114d27d6ea1f90
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2020-01-04 15:18:34 -0200

    Fix update

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import store from './store'
-import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vuetify from './plugins/vuetify';
+import vuelidate from './plugins/vuelidate';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  vuetify,
+  Vuetify,
+  vuelidate,
   render: h => h(App)
 }).$mount('#app')
